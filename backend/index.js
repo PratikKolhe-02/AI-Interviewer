3bda6389c62a48b33a21e3472d8498fb38a4c4aa
PratikKOlhe-02 <pratikkolhe02.official@gmail.com>
2025-12-12 14:43:07 +0530

    Day 1 Complete: Full Project Setup

---
+++ backend/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.get('/', (req, res) => res.send('Backend is Working!'));
+app.listen(5000, () => console.log('Server started on 5000'));
